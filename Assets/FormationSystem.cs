738248c31de353112af8b7a6f67f722626230ae5
unknown32767 <chenao960716@gmail.com>
2020-04-20 04:56:02 +0800

    伪平滑

--- Assets/FormationSystem.cs
+++ Assets/FormationSystem.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Configuration.Assemblies;
+using System.Data.Common;
 using System.Linq;
 using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
 
 public class FormationSystem : SystemBase
 {
@@ -18,21 +21,57 @@ public class FormationSystem : SystemBase
         Curve,
     };
 
-    private List<(SegmentType type, Vector3 node)> smoothPath;
+    private List<(SegmentType type, Vector3 start, Vector3 end)> smoothPath;
 
     protected override void OnCreate()
     {
         path = new NavMeshPath();
 
         radius = Spawner.offsets.Select(x => x.magnitude).Max();
+
+        smoothPath = new List<(SegmentType type, Vector3 start, Vector3 end)>();
     }
 
     private void SmoothPath()
     {
+        smoothPath.Clear();
+
         var corners = path.corners;
-        for (int i = 0; i < corners.Length; i++)
+
+        var curvePoints = new List<Vector3>();
+
+        for (int i = 0; i < corners.Length - 2; i++)
         {
-            
+            var p0 = corners[i];
+            var p1 = corners[i + 1];
+            var p2 = corners[i + 2];
+
+            var turnAngle = math.abs(math.radians(Vector3.SignedAngle(p1 - p0, p2 - p1, Vector3.up)));
+            var turnLength = radius / math.tan(0.5f * (math.PI - turnAngle));
+
+
+            Debug.Log(turnLength);
+            var q1 = p1 - (p1 - p0).normalized * turnLength;
+            var q2 = p1 + (p2 - p1).normalized * turnLength;
+
+            curvePoints.Add(q1);
+            curvePoints.Add(q2);
+        }
+
+        if (corners.Length == 2)
+        {
+            smoothPath.Add((SegmentType.Line, corners[0], corners[1]));
+        }
+        else
+        {
+            smoothPath.Add((SegmentType.Line, corners[0], curvePoints[0]));
+            for (int i = 0; i < corners.Length - 3; i++)
+            {
+                smoothPath.Add((SegmentType.Curve, curvePoints[i * 2], curvePoints[i * 2 + 1]));
+                smoothPath.Add((SegmentType.Line, curvePoints[i * 2 + 1], curvePoints[i * 2 + 2]));
+            }
+            smoothPath.Add((SegmentType.Curve, curvePoints[curvePoints.Count-2], curvePoints.Last()));
+            smoothPath.Add((SegmentType.Line, curvePoints.Last(), corners.Last()));
         }
     }
 
@@ -42,7 +81,39 @@ public class FormationSystem : SystemBase
 
         NavMesh.CalculatePath(start, end, NavMesh.AllAreas, path);
 
+        if (path.status == NavMeshPathStatus.PathComplete)
+        {
+            SmoothPath();
+        }
+
         currentSegment = 0;
+    }
+
+    private float SegmentLength(SegmentType type, Vector3 start, Vector3 end)
+    {
+        if (type == SegmentType.Line)
+        {
+            return Vector3.Distance(start, end);
+        }
+        else
+        {
+            var chordLength = Vector3.Distance(start, end);
+            return math.asin(chordLength * 0.5f / radius) * 2 * radius;
+        }
+    }
+
+    private float SegmentLength(int index)
+    {
+        var (type, start, end) = smoothPath[index];
+
+        return SegmentLength(type, start, end);
+    }
+
+    private float LeftLength(int index, float3 pos)
+    {
+        var (type, _, end) = smoothPath[index];
+
+        return SegmentLength(type, pos, end);
     }
 
     protected override void OnUpdate()
@@ -59,49 +130,98 @@ public class FormationSystem : SystemBase
         var rotation = formationCenter.rotation;
         var travelDistance = speed * Time.DeltaTime;
 
-        while (travelDistance > 0 && currentSegment < path.corners.Length - 1)
+        while (travelDistance > 0 && currentSegment < smoothPath.Count)
         {
-            var segmentLength = math.distance(nextPos, path.corners[currentSegment + 1]);
-
             //在该段路线的起点
-            if (nextPos == path.corners[currentSegment])
+            if (currentSegment == 0)
             {
-                //转向未完成
-                if (math.dot(math.normalize(math.mul(rotation, Vector3.forward)) , 
-                        math.normalize(path.corners[currentSegment + 1] - path.corners[currentSegment])) < 1)
+                if (nextPos == path.corners[currentSegment])
                 {
-                    var turnAngle = math.radians(Vector3.SignedAngle(math.mul(rotation, Vector3.forward),
-                        path.corners[currentSegment + 1] - path.corners[currentSegment], Vector3.up));
-
-                    var maxTurnAngle = travelDistance / radius;
-
-                    if (maxTurnAngle >= math.abs(turnAngle))
+                    //转向未完成
+                    if (math.dot(math.normalize(math.mul(rotation, Vector3.forward)),
+                            math.normalize(path.corners[currentSegment + 1] - path.corners[currentSegment])) < 1)
                     {
-                        rotation = math.normalize(quaternion.LookRotation(
+                        var turnAngle = math.radians(Vector3.SignedAngle(math.mul(rotation, Vector3.forward),
                             path.corners[currentSegment + 1] - path.corners[currentSegment], Vector3.up));
-                        travelDistance -= math.abs(turnAngle) * radius;
-                    }
-                    else
-                    {
-                        rotation = math.mul(rotation, quaternion.Euler(0, maxTurnAngle * math.sign(turnAngle), 0));
-                        travelDistance = 0;
-                        break;
+
+                        var maxTurnAngle = travelDistance / radius;
+
+                        if (maxTurnAngle >= math.abs(turnAngle))
+                        {
+                            rotation = math.normalize(quaternion.LookRotation(
+                                path.corners[currentSegment + 1] - path.corners[currentSegment], Vector3.up));
+                            travelDistance -= math.abs(turnAngle) * radius;
+                        }
+                        else
+                        {
+                            rotation = math.mul(rotation, quaternion.Euler(0, maxTurnAngle * math.sign(turnAngle), 0));
+                            travelDistance = 0;
+                            break;
+                        }
                     }
                 }
             }
 
-            //剩余可用路程长度在本段之内
+            var (type, start, end) = smoothPath[currentSegment];
+
+            var segmentLength = LeftLength(currentSegment, nextPos);
+            var index = (currentSegment + 1) / 2;
+
             if (travelDistance < segmentLength)
             {
-                nextPos = math.lerp(nextPos, path.corners[currentSegment + 1], travelDistance / segmentLength);
+
+                if (type == SegmentType.Line)
+                {
+                    nextPos = math.lerp(nextPos, end, travelDistance / segmentLength);
+                }
+                else
+                {
+                    var turnAngle = math.radians(Vector3.SignedAngle(math.mul(rotation, Vector3.forward),
+                        path.corners[index + 1] - path.corners[index], Vector3.up));
+                    var fullAngle = math.radians(Vector3.SignedAngle(path.corners[index - 1] - path.corners[index],
+                        path.corners[index + 1] - path.corners[index], Vector3.up));
+                    var maxTurnAngle = travelDistance / radius * 0.5f;
+
+                    var t = 1 - (math.abs(turnAngle) +maxTurnAngle) / math.abs(fullAngle);
+
+                    Debug.Log(t);
+
+                    rotation = math.mul(rotation, quaternion.Euler(0, maxTurnAngle * math.sign(turnAngle), 0));
+                    nextPos += (1 - t) * (1 - t) * start + 2 * t * (1 - t) * path.corners[index] + t * t * end;
+                }
+
                 travelDistance = 0;
             }
             else
             {
                 travelDistance -= segmentLength;
-                nextPos = path.corners[currentSegment + 1];
+                nextPos = end;
+
+                if (type == SegmentType.Curve)
+                {
+                    rotation = math.normalize(quaternion.LookRotation(path.corners[index + 1] - path.corners[index],
+                        Vector3.up));
+                }
+
                 currentSegment += 1;
             }
+
+            //var segmentLength = math.distance(nextPos, path.corners[currentSegment + 1]);
+
+
+
+            ////剩余可用路程长度在本段之内
+            //if (travelDistance < segmentLength)
+            //{
+            //    nextPos = math.lerp(nextPos, path.corners[currentSegment + 1], travelDistance / segmentLength);
+            //    travelDistance = 0;
+            //}
+            //else
+            //{
+            //    travelDistance -= segmentLength;
+            //    nextPos = path.corners[currentSegment + 1];
+            //    currentSegment += 1;
+            //}
         }
 
         SetSingleton(new FormationCenterComponent
@@ -111,11 +231,18 @@ public class FormationSystem : SystemBase
             rotation = rotation,
         });
 
-        for (int i = 0; i < path.corners.Length - 1; i++)
+        //for (int i = 0; i < path.corners.Length - 1; i++)
+        //{
+        //    Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.blue);
+        //}
+
+        foreach (var (type, start, end) in smoothPath)
         {
-            Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.blue);
+            Debug.DrawLine(start, end, type == SegmentType.Line ? Color.blue : Color.green);
         }
 
         Debug.DrawRay(nextPos, math.mul(rotation, Vector3.forward), Color.red);
+        Debug.DrawRay(nextPos, math.mul(rotation, Vector3.left), Color.red);
+        Debug.DrawRay(nextPos, math.mul(rotation, Vector3.right), Color.red);
     }
 }
